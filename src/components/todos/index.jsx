549357e6898df18547c44224ccfac5092f3c88c9
majedurwd <majedur.cse@gmail.com>
2023-03-20 22:15:11 +0600

    setup our project

---
+++ src/components/todos/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Todos extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2 className="display-5 text-center mb-5">Check Todos</h2>
+            </div>
+        )
+    }
+}
+
+export default Todos
